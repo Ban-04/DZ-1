2367e0f051e9da4463f11d65d50556d61600b286
Shol <tanksit-16@list.ru>
2025-11-23 20:40:54 +1000

    С бесконечной повторкой у врача. Умножим да поделим. Списывание.

--- Shveciar.py
+++ Shveciar.py
@@ -1,9 +1,8 @@
-while True:
- print("Умножим да поделим.Предлагаю вам написать всего лишь число")
- N= int(input())
- Answer =(N*(N+1))/2
- print(Answer)
- repeat=input("Еще раз ?")
- if repeat.strip().lower() =='нет':
-     break
-
+print("ssss")
+N=int(input())
+A=0
+B=0
+while A<N:
+    A +=1
+    B +=A
+    print(B)
